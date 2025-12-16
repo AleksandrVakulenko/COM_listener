5b38905d492b29ce4d0f24658e0c482da9e464ad
Aleksandr Vakulenko <sasha705@mail.ru>
2025-12-16 13:53:22 +0300

    move main_2 to main, move main to main_old_version

--- main.m
+++ main.m
@@ -1,58 +1,30 @@
 
-% V1.0
-%
-% TODO (2025.12.15):
-% 1) Add loging
-% 2) Add print to console
-% 3) Do some tests
+% Special type of callback for my Temp_controller_N2
 
 COM_port_host = "COM11"; % one side of com0com link
-COM_port_device = "COM3"; % real device port
-BAUD_rate = 230400; % same baud rate for both ports
+COM_port_device = "COM6"; % real device port
+BAUD_rate = 9600; % same baud rate for both ports
 
-% settings
-pause_time = 0.00; % [s]
 
-% init loop variables
+clc
+
+[ser_host, ser_device] = ...
+    link_serial_ports(COM_port_host, COM_port_device, BAUD_rate);
+
+
+% init loop variables and run main
 Bytes_from_host = 0;
 Bytes_from_device = 0;
 Time_arr = [];
 Bytes_from_host_arr = [];
 Bytes_from_device_arr = [];
-
-
-clc
-[ser_host, ser_device] = connect(COM_port_host, COM_port_device, BAUD_rate);
-
 try
     fig = create_figure();
-    drawnow
-    Timer_obj = tic;
     while(~figure_should_close(fig))
         Time = toc(Timer_obj);
-        
-        % get data
-        data_from_host = read_data(ser_host);
-        data_from_device = read_data(ser_device);
 
-        % send data (if any)
-        if ~isempty(data_from_device)
-            write(ser_host, data_from_device, "uint8");
-            disp(['DEV: (' num2str(numel(data_from_device)) ')' ...
-                num2str(data_from_device)])
-        end
-        if ~isempty(data_from_host)
-            write(ser_device, data_from_host, "uint8");
-            disp(['HOST: (' num2str(numel(data_from_host)) ')' ...
-                num2str(data_from_host)])
-        end
-    
-        if isempty(data_from_host) && isempty(data_from_device)
-            disp('no data')
-        end
-
-        Bytes_from_host = Bytes_from_host + numel(data_from_host);
-        Bytes_from_device = Bytes_from_device + numel(data_from_device);
+        Bytes_from_host = ser_host.UserData.bytes_counter;
+        Bytes_from_device = ser_device.UserData.bytes_counter;
 
         Time_arr = [Time_arr Time];
         Bytes_from_host_arr = [Bytes_from_host_arr Bytes_from_host];
@@ -60,8 +32,9 @@ try
 
         plot_number_of_bytes(fig, Time_arr, Bytes_from_host_arr, ...
             Bytes_from_device_arr);
+        
         drawnow
-%         pause(pause_time)
+        pause(0.001)
     end
 catch err
     delete(ser_host);
@@ -71,6 +44,8 @@ catch err
     rethrow(err);
 end
 
+Host_data_final = ser_host.UserData;
+Device_data_final = ser_device.UserData;
 
 delete(fig)
 disp('finish (main)')
@@ -80,64 +55,111 @@ delete(ser_device);
 disp('Both serial obj closed')
 
 
+%% Serial and Callback part
+
+function [ser_host, ser_device] = ...
+    link_serial_ports(COM_port_host, COM_port_device, BAUD_rate)
+
+    ser_host = serialport(COM_port_host, BAUD_rate);
+    
+    try
+        ser_device = serialport(COM_port_device, BAUD_rate);
+    catch err
+        delete(ser_host);
+        rethrow(err)
+    end
+    disp("Serial ports ready")
+    
+    try
+        configureCallback(ser_host, "byte", 5, @BytesAvCallback);
+        configureCallback(ser_device, "byte", 1, @BytesAvCallback);
+    catch err
+        delete(ser_host);
+        delete(ser_device);
+        rethrow(err)
+    end
+    disp("Callback ready")
 
 
+    % init timer
+    Timer_obj = tic;
 
-
-%% Serial part
-function [ser_host, ser_device] = connect(COM_port_host, COM_port_device, BAUD_rate)
-ser_host = serialport(COM_port_host, BAUD_rate);
-try
-    ser_device = serialport(COM_port_device, BAUD_rate);
-catch err
-    detele(ser_host);
-    rethrow(err)
+    % init UserData for both serial obj
+    Host_data = struct('name', "host", ...
+                       'link', ser_device, ...
+                       'timer', Timer_obj, ...
+                       'bytes_counter', 0, ...
+                       'log', []);
+    
+    Device_data = struct('name', "device", ...
+                         'link', ser_host, ...
+                         'timer', Timer_obj, ...
+                         'bytes_counter', 0, ...
+                         'log', []);
+    
+    % set UserData
+    ser_host.UserData = Host_data;
+    ser_device.UserData = Device_data;
 end
-disp("Serial ports ready")
-end
 
 
-
-function data = read_data(ser_obj)
+function BytesAvCallback(ser_obj, ~)
+    Data = ser_obj.UserData;
+    Name = char(Data.name);
+    ser_link = Data.link;
+    Timer_obj = Data.timer;
+    
+    Time = toc(Timer_obj);
+    
     N = ser_obj.NumBytesAvailable;
     if N > 0
-        data = uint8(ser_obj.read(N, "uint8"));
-    else
-        data = uint8([]);
+        data = ser_obj.read(N, "uint8");
+        data = uint8(data);
+    
+        write(ser_link, data, "uint8");
+        disp([Name ' <' num2str(N) '>: ' num2str(data)])
+    
+        log_record = struct('name', Name, ...
+                            'time', Time, ...
+                            'data', data);
+    
+        ser_obj.UserData.log = [ser_obj.UserData.log log_record];
+        ser_obj.UserData.bytes_counter = ser_obj.UserData.bytes_counter + N;
     end
+
 end
+
+
+
+
+
+
+
 
 %% Figure part
 
-
 function fig = create_figure()
-res = get(0, 'ScreenSize');
-X_pos = res(3)*0.20;
-Y_pos = res(4)*0.32;
-X_size = res(3)*0.35;
-Y_size = res(4)*0.33;
-
-fig = figure('Position', [X_pos Y_pos X_size Y_size]);
-button_exit = uicontrol("Style", "pushbutton", "String", "Exit", ...
-    "Callback", @exit_foo, "UserData", struct('stop', false), ...
-    "Position", [20,20,82.5,33.5]);
-fig.UserData = struct('Exit_btn', button_exit);
-
+    res = get(0, 'ScreenSize');
+    X_pos = res(3)*0.20;
+    Y_pos = res(4)*0.32;
+    X_size = res(3)*0.35;
+    Y_size = res(4)*0.33;
+    
+    fig = figure('Position', [X_pos Y_pos X_size Y_size]);
+    button_exit = uicontrol("Style", "pushbutton", "String", "Exit", ...
+        "Callback", @exit_foo, "UserData", struct('stop', false), ...
+        "Position", [20,20,82.5,33.5]);
+    fig.UserData = struct('Exit_btn', button_exit);
+    
+    end
+    
+    function flag = figure_should_close(fig)
+        flag = fig.UserData.Exit_btn.UserData.stop;
+    end
+        
+    function exit_foo(a, ~)
+    a.UserData.stop = true;
 end
-
-function flag = figure_should_close(fig)
-    flag = fig.UserData.Exit_btn.UserData.stop;
-end
-
-
-function exit_foo(a, ~)
-a.UserData.stop = true;
-end
-
-
-
-
-
 
 
 function plot_number_of_bytes(fig, Time, Num_bytes_from_host, ...
@@ -146,10 +168,7 @@ function plot_number_of_bytes(fig, Time, Num_bytes_from_host, ...
         figure(fig);
 
         subplot('Position', [0.08 0.45 0.38 0.5]);
-        cla
-        hold on
         plot(Time, Num_bytes_from_host, '-b')
-        plot(Time, Num_bytes_from_device, '-r')
         ylabel('bytes from host')
         xlabel('time, s')
         set(gca, 'fontsize', 10)
@@ -160,6 +179,49 @@ function plot_number_of_bytes(fig, Time, Num_bytes_from_host, ...
         xlabel('time, s')
         set(gca, 'fontsize', 10)
 end
+
+
+
+
+
+
+
+%% UNUSED
+
+function BytesAvCallback_special_UNUSED(ser_obj, ~)
+    Data = ser_obj.UserData;
+    Name = char(Data.name);
+    ser_link = Data.link;
+    Timer_obj = Data.timer;
+    
+    Time = toc(Timer_obj);
+    
+    N = ser_obj.NumBytesAvailable;
+    if N > 0
+        data = ser_obj.read(N, "uint8");
+        data = uint8(data);
+        if Name == "host"
+            K = N/5;
+            for i = 1:K
+                range = ((i-1)*5:(i*5-1))+1;
+                write(ser_link, data(range), "uint8");
+                disp([Name ' <' num2str(5) '>: ' num2str(data(range))])
+                pause(0.001)
+            end
+        else
+            write(ser_link, data, "uint8");
+            disp([Name ' <' num2str(N) '>: ' num2str(data)])
+        end
+        
+        log_record = struct('name', Name, ...
+                            'time', Time, ...
+                            'data', data);
+    
+        ser_obj.UserData.log = [ser_obj.UserData.log log_record];
+        ser_obj.UserData.bytes_counter = ser_obj.UserData.bytes_counter + N;
+    end
+end
+
 
 
 
---
+++ main_old_version.m
@@ -0,0 +1,168 @@
+
+% V1.0
+%
+% TODO (2025.12.15):
+% 1) Add loging
+% 2) Add print to console
+% 3) Do some tests
+
+COM_port_host = "COM11"; % one side of com0com link
+COM_port_device = "COM3"; % real device port
+BAUD_rate = 230400; % same baud rate for both ports
+
+% settings
+pause_time = 0.00; % [s]
+
+% init loop variables
+Bytes_from_host = 0;
+Bytes_from_device = 0;
+Time_arr = [];
+Bytes_from_host_arr = [];
+Bytes_from_device_arr = [];
+
+
+clc
+[ser_host, ser_device] = connect(COM_port_host, COM_port_device, BAUD_rate);
+
+try
+    fig = create_figure();
+    drawnow
+    Timer_obj = tic;
+    while(~figure_should_close(fig))
+        Time = toc(Timer_obj);
+        
+        % get data
+        data_from_host = read_data(ser_host);
+        data_from_device = read_data(ser_device);
+
+        % send data (if any)
+        if ~isempty(data_from_device)
+            write(ser_host, data_from_device, "uint8");
+            disp(['DEV: (' num2str(numel(data_from_device)) ')' ...
+                num2str(data_from_device)])
+        end
+        if ~isempty(data_from_host)
+            write(ser_device, data_from_host, "uint8");
+            disp(['HOST: (' num2str(numel(data_from_host)) ')' ...
+                num2str(data_from_host)])
+        end
+    
+        if isempty(data_from_host) && isempty(data_from_device)
+            disp('no data')
+        end
+
+        Bytes_from_host = Bytes_from_host + numel(data_from_host);
+        Bytes_from_device = Bytes_from_device + numel(data_from_device);
+
+        Time_arr = [Time_arr Time];
+        Bytes_from_host_arr = [Bytes_from_host_arr Bytes_from_host];
+        Bytes_from_device_arr = [Bytes_from_device_arr Bytes_from_device];
+
+        plot_number_of_bytes(fig, Time_arr, Bytes_from_host_arr, ...
+            Bytes_from_device_arr);
+        drawnow
+%         pause(pause_time)
+    end
+catch err
+    delete(ser_host);
+    delete(ser_device);
+    delete(fig)
+    disp('Both serial obj closed (in catch)')
+    rethrow(err);
+end
+
+
+delete(fig)
+disp('finish (main)')
+
+delete(ser_host);
+delete(ser_device);
+disp('Both serial obj closed')
+
+
+
+
+
+
+%% Serial part
+function [ser_host, ser_device] = connect(COM_port_host, COM_port_device, BAUD_rate)
+ser_host = serialport(COM_port_host, BAUD_rate);
+try
+    ser_device = serialport(COM_port_device, BAUD_rate);
+catch err
+    detele(ser_host);
+    rethrow(err)
+end
+disp("Serial ports ready")
+end
+
+
+
+function data = read_data(ser_obj)
+    N = ser_obj.NumBytesAvailable;
+    if N > 0
+        data = uint8(ser_obj.read(N, "uint8"));
+    else
+        data = uint8([]);
+    end
+end
+
+%% Figure part
+
+
+function fig = create_figure()
+res = get(0, 'ScreenSize');
+X_pos = res(3)*0.20;
+Y_pos = res(4)*0.32;
+X_size = res(3)*0.35;
+Y_size = res(4)*0.33;
+
+fig = figure('Position', [X_pos Y_pos X_size Y_size]);
+button_exit = uicontrol("Style", "pushbutton", "String", "Exit", ...
+    "Callback", @exit_foo, "UserData", struct('stop', false), ...
+    "Position", [20,20,82.5,33.5]);
+fig.UserData = struct('Exit_btn', button_exit);
+
+end
+
+function flag = figure_should_close(fig)
+    flag = fig.UserData.Exit_btn.UserData.stop;
+end
+
+
+function exit_foo(a, ~)
+a.UserData.stop = true;
+end
+
+
+
+
+
+
+
+function plot_number_of_bytes(fig, Time, Num_bytes_from_host, ...
+    Num_bytes_from_device)
+            
+        figure(fig);
+
+        subplot('Position', [0.08 0.45 0.38 0.5]);
+        cla
+        hold on
+        plot(Time, Num_bytes_from_host, '-b')
+        plot(Time, Num_bytes_from_device, '-r')
+        ylabel('bytes from host')
+        xlabel('time, s')
+        set(gca, 'fontsize', 10)
+
+        subplot('Position', [0.56 0.45 0.38 0.5]);
+        plot(Time, Num_bytes_from_device)
+        ylabel('bytes from device')
+        xlabel('time, s')
+        set(gca, 'fontsize', 10)
+end
+
+
+
+
+
+
